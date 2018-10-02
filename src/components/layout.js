23246c0ff0823421846c9fd2a2a2791274619e09
Gareth Bk <garethbk@gmail.com>
2018-10-02 13:40:16 -0700

    Initial under construction commit

---
+++ src/components/layout.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { StaticQuery, graphql } from 'gatsby';
+
+import './layout.css';
+import './svg.css';
+
+import Favicon from '../images/favicon.ico';
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet title={data.site.siteMetadata.title}>
+          <link rel="shortcut icon" href={Favicon} />
+          <html lang="en" />
+        </Helmet>
+        <div>{children}</div>
+      </>
+    )}
+  />
+);
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '../components/layout';
+import 'typeface-fahkwang';
+import 'typeface-source-code-pro';
+
+const IndexPage = () => (
+  <Layout>
+    <div className="text">
+      <h1 className="title">
+        <em>GARETH.COOL</em>
+      </h1>
+      <h2 className="subtitle">(is under construction)</h2>
+    </div>
+  </Layout>
+);
+
+export default IndexPage;
